2c4cd5ce3c78444a05e15d01f3f5b1bbab7e999d
puriravindra412 <70501907+puriravindra412@users.noreply.github.com>
2022-09-30 11:39:40 +0530

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -28,7 +28,7 @@ function Projects() {
               title="Personal Portfolio"
               description="My personal portfolio which features some of my github projects as well as my resume and technical skills."
               ghLink="https://github.com/puriravindra412/Personal-Portfolio"
-              demoLink="https://puriravindra412.github.io/"
+              demoLink="https://puriravindra412.github.io/Personal-Portfolio/"
             />
           </Col>
 
